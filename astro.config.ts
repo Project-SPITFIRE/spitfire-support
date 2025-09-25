0616b364072503f90dd1b45ad4445e3301de24d8
AlonzoA2K <alonzo.pamintuan@a2kgroup.co.uk>
2025-09-25 15:59:48 +0800

    style: change theme

--- astro.config.ts
+++ astro.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   integrations: [
     starlight({
       plugins: [
-        starlightThemeObsidian(),
+        starlightThemeRapide(), // change theme here..
         starlightSidebarTopics([
           {
             label: "Welcome",
