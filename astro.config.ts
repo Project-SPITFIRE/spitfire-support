4d72e2d6836d272cc4058a821433cfcf3dbeeff3
AlonzoA2K <alonzo.pamintuan@a2kgroup.co.uk>
2025-10-02 14:00:35 +0800

    docs: refactor structure

--- astro.config.ts
+++ astro.config.ts
@@ -1,134 +1,82 @@
+// Starlight Default Imports:
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import tailwindcss from "@tailwindcss/vite";
+
+// Utilities:
+import starlightContextualMenu from "starlight-contextual-menu";
+import starlightSidebarSwipe from "starlight-sidebar-swipe";
+import starlightImageZoom from "starlight-image-zoom";
+import icon from "astro-icon";
+
+import { viewTransitions } from "astro-vtbot/starlight-view-transitions";
+import starlightUtils from "@lorenzo_lewis/starlight-utils";
+import astroD2 from "astro-d2";
+
+
+// Themes:
 import starlightThemeRapide from "starlight-theme-rapide";
 import starlightThemeObsidian from "starlight-theme-obsidian";
 import starlightThemeGalaxy from "starlight-theme-galaxy";
 import { ion } from "starlight-ion-theme";
-import starlightSidebarTopics from "starlight-sidebar-topics";
-import starlightUtils from "@lorenzo_lewis/starlight-utils";
-import starlightContextualMenu from "starlight-contextual-menu";
-import starlightSidebarSwipe from "starlight-sidebar-swipe";
-import { viewTransitions } from "astro-vtbot/starlight-view-transitions";
-import starlightImageZoom from "starlight-image-zoom";
 
-import astroD2 from "astro-d2";
 
 //https://astro.build/config
 export default defineConfig({
-
   integrations: [
+    icon(),
     starlight({
       plugins: [
-        //starlightThemeGalaxy(), // change theme here..
-        viewTransitions(),
+        starlightThemeRapide(), // change theme here..
+        // viewTransitions(),
         starlightImageZoom(),
         starlightSidebarSwipe(),
-
         starlightContextualMenu({
           actions: ["copy", "view"],
         }),
-        starlightSidebarTopics(
-        [
-          {
-            label: "Welcome",
-            link: "/welcome/getting-started",
-            icon: "rocket",
-            items: [
-              { label: "Start Here", slug: "welcome/getting-started" },
-            ],
-          },
-          {
-            label: "Roles",
-            link: "/roles/admin/create-user-accounts",
-            icon: "open-book",
-            items: [
-              {
-                label: "Admin",
-                items: [
-                  { label: "Admin Overview", slug: "roles/admin/role-overview" },
-                  { label: "Create User Accounts", slug: "roles/admin/create-user-accounts" },
-                  { label: "User Management", slug: "roles/admin/user-management" },
-                  { label: "Using the Configuration Centre", slug: "roles/admin/configuration-centre" },
-                  { label: "Team Management",
-                    items: [
-                      { label: "Create Teams", slug: "roles/admin/team-management/create-teams" },
-                      { label: "Delete Teams", slug: "roles/admin/team-management/delete-teams",},
-                    ],
-                  },
-                ],
-              },
-             {
-                label: "Ops-Manager",
-                items: [
-                  { label: "Ops-Manager Overview", slug: "roles/ops-manager/role-overview" },
-                  { label: "Scheduling", 
-                    items: [
-                      { label: "Submit Request", slug: "roles/ops-manager/scheduling/submit-request" },
-                      { label: "Update Request", slug: "roles/ops-manager/scheduling/update-request" },
-                      { label: "Schedules", slug: "roles/ops-manager/scheduling/schedules"},
-                      { label: "Rota Management",
-                        items: [
-                          {label: "Rota Management Overview", slug: "roles/ops-manager/scheduling/rota-management/rota-management-overview"},
-                          {label: "Events Mapping", slug: "roles/ops-manager/scheduling/rota-management/events-mapping" },
-                          {label: "Contract Allocation", slug: "roles/ops-manager/scheduling/rota-management/contract-allocation"},
-                          {label: "Succession Planning", slug: "roles/ops-manager/scheduling/rota-management/succession-planning" },
-                        ],
-                      },
-                    ],
-                  },
-                  {label: "Management", 
-                    items: [
-                      { label: "View EOS Tasks", slug: "roles/ops-manager/management/view-eos-tasks" },
-                      { label: "Update EOS Tasks", slug: "roles/ops-manager/management/update-eos-tasks" },
-                    ],
-                  },
-                ],
-              },
-              {
-                label: "Ops Support",
-                items: [
-                  { label: "Ops Support Overview", slug: "roles/ops-support/role-overview"},
-                  { label: "Dashboard", slug: "roles/ops-support/ops-support-dashboard"},
-                  { label: "Operational Tracker", slug: "roles/ops-support/ops-support-operational-tracker" },
-                  { label: "CIPHR Integration", slug: "roles/ops-support/ops-support-ciphr-integration" },
-                  { label: "Lieu & Holidays", slug: "roles/ops-support/ops-support-holidays-lieu" },
-                  { label: "Submit Requests", slug: "roles/ops-support/ops-support-submit-requests" },
-                  { label: "Submitted Schedules", slug: "roles/ops-support/ops-support-submitted-schedules" },
-                  { label: "Schedules", slug: "roles/ops-support/ops-support-schedules" },
-                  { label: "Allocated Contracts", slug: "roles/ops-support/ops-support-allocated-contracts" },
-                  { label: "Assigned Events", slug: "roles/ops-support/ops-support-assigned-events" },
-                  { label: "EOS Actions", slug: "roles/ops-support/ops-support-eos-action"},
-                  { label: "Manage Request", slug: "roles/ops-support/ops-support-manage-request" },
-                  { label: "Account Management", slug: "roles/ops-support/ops-support-account-management" },
-                  { label: "Create User Accounts", slug: "roles/ops-support/ops-support-create-account" },
-                  { label: "Team Management", slug: "roles/ops-support/ops-support-team-management" },
-                  { label: "Configuration Centre", slug: "roles/ops-support/ops-support-configuration-centre"},
-                  { label: "Rota Management",
-                    items: [
-                      { label: "Events Mapping", slug: "roles/ops-support/ops-support-events-mapping" },
-                      { label: "Contract Allocation", slug: "roles/ops-support/ops-support-contract-allocation" },
-                      { label: "Succession Planning", slug: "roles/ops-support/ops-support-succession-planning" },
-                    ],
-                  },
-
-                ],
-              },
-            ],
-            
-          },
-        ],
-      ),
-
       ],
       title: "Spitfire Docs",
       logo: {
         src: "@/assets/csp-logo.svg",
         alt: "Spitfire Logo",
       },
-
-
-      
+      sidebar: [
+        {
+          label: "Getting Started",
+          items: [
+            {label: "Introduction", slug: "getting-started/introduction" },
+            {label: "System Overview" , slug: "getting-started/system-overview" },
+            {label: "Authentication", slug: "getting-started/authentication" },
+            {label: "First Steps", slug: "getting-started/first-steps" },
+          ],
+        },
+        {
+          label: "Roles",
+          items: [
+            { label: "Admin", slug: "roles/admin" },
+            { label: "Operations Manager", slug: "roles/ops-manager" },
+            { label: "Operations Support", slug: "roles/ops-support" },
+          ],
+        },
+        {
+          label: "Features",
+          items: [
+            { label: "Request Management", slug: "features/request-management" },
+            { label: "EOS Tasks", slug: "features/eos-tasks" },
+            { label: "Schedules", slug: "features/schedules" },
+            { label: "Rota Management", slug: "features/rota-management" },
+            { label: "User & Team Management", slug: "features/user-team-management" },
+          ],
+        },
+        {
+          label: "References",
+          items: [
+            { label: "Glossary", slug: "references/glossary" },
+            { label: "Status Definitions", slug: "references/status-definitions" },
+            { label: "Troubleshooting", slug: "references/troubleshooting" },
+          ]
+        }
+      ],
       customCss: [
         "./src/styles/global.css",
         "@fontsource-variable/atkinson-hyperlegible-next",
