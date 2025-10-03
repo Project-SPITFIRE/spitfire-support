1a53a141e89f57b67d66cfda6325ff786b459f13
AlonzoA2K <alonzo.pamintuan@a2kgroup.co.uk>
2025-10-03 10:02:50 +0800

    docs: change hero title

--- astro.config.ts
+++ astro.config.ts
@@ -13,13 +13,12 @@ import { viewTransitions } from "astro-vtbot/starlight-view-transitions";
 import starlightUtils from "@lorenzo_lewis/starlight-utils";
 import astroD2 from "astro-d2";
 
-
 // Themes:
 import starlightThemeRapide from "starlight-theme-rapide";
 import starlightThemeObsidian from "starlight-theme-obsidian";
 import starlightThemeGalaxy from "starlight-theme-galaxy";
+import starlightThemeNova from "starlight-theme-nova";
 import { ion } from "starlight-ion-theme";
-
 
 //https://astro.build/config
 export default defineConfig({
@@ -35,7 +34,7 @@ export default defineConfig({
           actions: ["copy", "view"],
         }),
       ],
-      title: "Spitfire Docs",
+      title: "Standard CSP Rota - Learning Materials",
       logo: {
         src: "@/assets/csp-logo.svg",
         alt: "Spitfire Logo",
@@ -44,16 +43,19 @@ export default defineConfig({
         {
           label: "Getting Started",
           items: [
-            {label: "Introduction", slug: "getting-started/introduction" },
-            {label: "System Overview" , slug: "getting-started/system-overview" },
-            {label: "Authentication", slug: "getting-started/authentication" },
-            {label: "First Steps", slug: "getting-started/first-steps" },
+            { label: "Introduction", slug: "getting-started/introduction" },
+            {
+              label: "System Overview",
+              slug: "getting-started/system-overview",
+            },
+            { label: "Authentication", slug: "getting-started/authentication" },
+            { label: "First Steps", slug: "getting-started/first-steps" },
           ],
         },
         {
           label: "Roles",
           items: [
-            { label: "Role Overview", slug: "roles/overview"},
+            { label: "Role Overview", slug: "roles/overview" },
             { label: "Admin", slug: "roles/admin" },
             { label: "Operations Manager", slug: "roles/ops-manager" },
             { label: "Operations Support", slug: "roles/ops-support" },
@@ -62,28 +64,49 @@ export default defineConfig({
         {
           label: "Features",
           items: [
-            { label: "Request Management", slug: "features/request-management" },
+            {
+              label: "Request Management",
+              slug: "features/request-management",
+            },
             { label: "EOS Tasks", slug: "features/eos-tasks" },
             { label: "Schedules", slug: "features/schedules" },
             { label: "Rota Management", slug: "features/rota-management" },
-            { label: "Contract Allocation", slug: "features/contract-allocation" },
-            { label: "Allocated Contracts", slug: "features/allocated-contracts"},
+            {
+              label: "Contract Allocation",
+              slug: "features/contract-allocation",
+            },
+            {
+              label: "Allocated Contracts",
+              slug: "features/allocated-contracts",
+            },
             { label: "Assigned Events", slug: "features/assigned-events" },
             { label: "Event Mapping", slug: "features/event-mapping" },
-            { label: "Succession Planning", slug: "features/succession-planning"},
-            { label: "Create User Accounts", slug: "features/create-user"},
-            { label: "User & Team Management", slug: "features/user-team-management" },
-            { label: "Configuration Centre", slug: "features/configuration-centre" },
+            {
+              label: "Succession Planning",
+              slug: "features/succession-planning",
+            },
+            { label: "Create User Accounts", slug: "features/create-user" },
+            {
+              label: "User & Team Management",
+              slug: "features/user-team-management",
+            },
+            {
+              label: "Configuration Centre",
+              slug: "features/configuration-centre",
+            },
           ],
         },
         {
           label: "References",
           items: [
             { label: "Glossary", slug: "references/glossary" },
-            { label: "Status Definitions", slug: "references/status-definitions" },
+            {
+              label: "Status Definitions",
+              slug: "references/status-definitions",
+            },
             { label: "Troubleshooting", slug: "references/troubleshooting" },
-          ]
-        }
+          ],
+        },
       ],
       customCss: [
         "./src/styles/global.css",
